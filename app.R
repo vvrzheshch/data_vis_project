c63577cef4605f27d0c55233233589976d840896
Valentin Vrzheshch <vv@Valentins-MacBook-Pro.local>
2017-05-07 21:15:33 -0700

    Alice!

--- app.R
+++ app.R
@@ -17,28 +17,36 @@ library(GGally)
 library(googleVis)
 library(googleCharts) # More info:  https://github.com/jcheng5/googleCharts Install: devtools::install_github("jcheng5/googleCharts")
 library(plyr)
+library(treemap)
+library(d3treeR)
 
 
-#setwd('~/Google Drive/MSAN2017/SPRING_2017/MSAN-622-02_Data_and_Information_Visualization/project/data_vis_project/')
-setwd('~/workdata/data_vis/data_vis_project/')
+setwd('~/Google Drive/MSAN2017/SPRING_2017/MSAN-622-02_Data_and_Information_Visualization/project/data_vis_project/')
+# setwd('~/workdata/data_vis/data_vis_project/')
 
 df <- read.csv('trade.csv',stringsAsFactors = FALSE)
-df_export <- df[df$Flow_Code == 'X', ]
-df_import <- df[df$Flow_Code == 'M', ]
-colnames(df_import)[1:4] <- c("Partner_code","Partner_description","Reporter_code","Reporter_description")
 
-new_df <- rbind(df_export,df_import)
+# Convert names to ASCII
+df$Reporter_description <-iconv(df$Reporter_description, "latin1", "ASCII", "")  
 
-new_df_grouped <- aggregate(Value~Reporter_description+Partner_description+Year,new_df,sum)
+# Prepare data for Tree Map
+  df_tree <- df[df$Indicator_description != "Total merchandise", c("Reporter_description", "Indicator_description", "Flow_Description", "Year", "Value")]
+  df_tree <- aggregate(Value ~ Reporter_description + Indicator_description + Flow_Description + Year,
+                       df_tree, sum)
+  
 
-new_df_grouped <- new_df_grouped[new_df_grouped$Reporter_description != new_df_grouped$Partner_description,]
-new_df_grouped <- new_df_grouped[new_df_grouped$Partner_description != 'World',]
-non_countries <- c("ACP (Africa, Caribbean and Pacific Countries)", "Africa, CIS and Middle East", "Andean Community", "American Samoa", "APEC (Asia-Pacific Economic Cooperation)", "Aruba (the Netherlands with respect to)", "Asia", "Asia excluding Hong Kong re-exports", "Asia less JPN,ANZ,CHN,NICS4, IN", "Australia and New Zealand", "BRIC members", "BRICS members", "CACM (Central American Common Market)", "CARICOM (Caribbean Community)",  "CEMAC (Economic and Monetary Community of Central Africa)", "Chinese Taipei", "COMESA (Common Market for Eastern and Southern Africa)", "Commonwealth of Independent States (CIS)", "Czech and Slovak Fed. Rep., former", "Developing and Emerging Economies", "ECCAS (Economic Community of Central African States)", "ECOWAS (Economic Community of West African States)", "EFTA (European Free Trade Association)", "Europe (Indices only)", "Europe excluding EU(28) intra-trade", "European Union (12)", "European Union (15)", "European Union (27)", "European Union (28)", "Four East Asian traders", "French Southern and Antarctic Territory", "French Guiana", "French Polynesia", "G20 - Developed Economies", "G20 - Developed Economies excl EU", "G20 - Developing economies", "G20 Members", "G20 Members exc. EU incl.FR, DE,IT,UK", "GCC (Gulf Co-operation Council)", "German Dem. Rep., former", "LDC (Least developed countries)", "LDC exporters of agriculture", "LDC exporters of manufactures", "LDC oil exporters", "MERCOSUR (Southern Common Market)", "Middle East", "NAFTA (North American Free Trade Agreement)", "Netherlands Antilles", "North America", "SADC (Southern African Development Community)", "SAFTA (South Asian Free Trade Agreement)", "South Africa", "South America excluding Brazil", "South and Central America", "Switzerland (Excl. Gold)", "WAEMU (West African Economic and Monetary Union)", "World", "World (only indices- excluding HK RX and CH Gold)", "World excluding EU(28) intra-trade", "WTO Members 2015", "WTO Members 2015 Incl. HK RX", "Developing Asia excluding Hong Kong re-exports", "ASEAN (Association of South East Asian Nations)",'Other CIS (CIS11)')
-new_df_grouped <- new_df_grouped[!(new_df_grouped$Reporter_description %in% non_countries),]
-df_sankey <- new_df_grouped[!(new_df_grouped$Partner_description %in% non_countries),]
-df_sankey <- na.omit(df_sankey)
-# Clean up the data for Sankey plot (remove regions to avoid cycling)
-df_sankey$Partner_description <- paste0(df_sankey$Partner_description,' ')
+# Prepare data for Sankey plot
+  df_export <- df[df$Flow_Code == 'X' & df$Indicator_description == "Total merchandise" & df$Partner_description != 'World', ]
+  df_import <- df[df$Flow_Code == 'M' & df$Indicator_description == "Total merchandise" & df$Partner_description != 'World', ]
+  colnames(df_import)[1:4] <- c("Partner_code","Partner_description","Reporter_code","Reporter_description")
+  df_sankey <-  aggregate(Value~Reporter_description+Partner_description+Year,
+                              rbind(df_export,df_import),
+                              sum)
+  non_countries <- c("World", "Extra-trade", "Other Africa", "Other Europe", "Other Asia", "Africa", "Europe", "ACP (Africa, Caribbean and Pacific Countries)", "Africa, CIS and Middle East", "Andean Community", "American Samoa", "APEC (Asia-Pacific Economic Cooperation)", "Aruba (the Netherlands with respect to)", "Asia", "Asia excluding Hong Kong re-exports", "Asia less JPN,ANZ,CHN,NICS4, IN", "Australia and New Zealand", "BRIC members", "BRICS members", "CACM (Central American Common Market)", "CARICOM (Caribbean Community)",  "CEMAC (Economic and Monetary Community of Central Africa)", "Chinese Taipei", "COMESA (Common Market for Eastern and Southern Africa)", "Commonwealth of Independent States (CIS)", "Czech and Slovak Fed. Rep., former", "Developing and Emerging Economies", "ECCAS (Economic Community of Central African States)", "ECOWAS (Economic Community of West African States)", "EFTA (European Free Trade Association)", "Europe (Indices only)", "Europe excluding EU(28) intra-trade", "European Union (12)", "European Union (15)", "European Union (27)", "European Union (28)", "Four East Asian traders", "French Southern and Antarctic Territory", "French Guiana", "French Polynesia", "G20 - Developed Economies", "G20 - Developed Economies excl EU", "G20 - Developing economies", "G20 Members", "G20 Members exc. EU incl.FR, DE,IT,UK", "GCC (Gulf Co-operation Council)", "German Dem. Rep., former", "LDC (Least developed countries)", "LDC exporters of agriculture", "LDC exporters of manufactures", "LDC oil exporters", "MERCOSUR (Southern Common Market)", "Middle East", "NAFTA (North American Free Trade Agreement)", "Netherlands Antilles", "North America", "SADC (Southern African Development Community)", "SAFTA (South Asian Free Trade Agreement)", "South Africa", "South America excluding Brazil", "South and Central America", "Switzerland (Excl. Gold)", "WAEMU (West African Economic and Monetary Union)", "World", "World (only indices- excluding HK RX and CH Gold)", "World excluding EU(28) intra-trade", "WTO Members 2015", "WTO Members 2015 Incl. HK RX", "Developing Asia excluding Hong Kong re-exports", "ASEAN (Association of South East Asian Nations)",'Other CIS (CIS11)')
+  non_countries_2 <- paste0(non_countries,' ')
+  # df_sankey <- na.omit(df_sankey)
+  df_sankey$Partner_description <- paste0(df_sankey$Partner_description,' ')
+  # df_sankey <- df_sankey[order(df_sankey$Reporter_description), ]
 
 
 # Define UI for application that plot the data
@@ -48,9 +56,18 @@ ui <- fluidPage(
        tabsetPanel(
          # tabPanel("bubble plot", plotlyOutput("Bubble.Plot")),
          # tabPanel("parallel coordinates plot", plotOutput("par_plot")),
-         tabPanel("dataset", uiOutput("description")),
-         tabPanel("Tables", fluidRow(dataTableOutput('table'))),
-         tabPanel("Countries to Regions Exports", htmlOutput("sankey.plot"))
+         # tabPanel("dataset", uiOutput("description")),
+         # tabPanel("Tables", fluidRow(dataTableOutput('table'))),
+         tabPanel("Countries to Regions Exports", 
+                  radioButtons("radio.left", "Left:", c("Countries" = "Countries", "Regions" = "Regions"), selected="Countries"),
+                  radioButtons("radio.right", "Right:", c("Countries" = "Countries", "Regions" = "Regions"), selected="Regions"),
+                  htmlOutput("sankey.plot")),
+         tabPanel("Tree Map of International Trade",
+                  # radioButtons("radio.indicator", "Indicator:", c(1,2)),
+                  radioButtons("radio.from", "From:", unique(df_tree$Reporter_description)),
+                  # radioButtons("radio.indicator", "Indicator:", unique(df_tree$Indicator_description)),
+                  # radioButtons("radio.flow", "Flow:", unique(df_tree$Flow_description)),
+                  d3tree2Output("d3tree"))
          
          # tabPanel("Google Chart",
          #          # This line loads the Google Charts JS library
@@ -72,9 +89,9 @@ ui <- fluidPage(
      fluidRow(
        shiny::column(4, offset = 4,
                      sliderInput("year", "Year", min = min(df$Year), max = max(df$Year),
-                                 value = max(df_sankey$Year)-5, animate = TRUE, step = 1, sep = ""),
-           selectInput("x", label = h3("Select x-axis:"), choices = unique(df$Reporter_description)), #choices = unique(df$Subject)
-           selectInput("y", label = h3("Select y-axis:"), choices = unique(df$Partner_description))#, selected = "Employment")
+                                 value = max(df_sankey$Year)-5, animate = TRUE, step = 1, sep = "")
+           # selectInput("x", label = h3("Select x-axis:"), choices = unique(df$Reporter_description)), #choices = unique(df$Subject)
+           # selectInput("y", label = h3("Select y-axis:"), choices = unique(df$Partner_description))#, selected = "Employment")
        )
      )
 )
@@ -93,14 +110,25 @@ ui <- fluidPage(
 # Define server logic required to plot the dataset
 server <- function(input, output) {
  # sub_df <- reactive({df_sankey[df_sankey$Year == input$year, ]})
+  
+  sub_df_tree <- reactive({
+    df_tree[df_tree$Year == input$year, ]
+  })
+  
   sub_df_sankey <- reactive({
-    df_sankey_year <- na.omit(df_sankey[df_sankey$Year == input$year, c('Reporter_description','Partner_description','Value')])
-    # df_sankey_year <- df_sankey[df_sankey$Year == input$year, c('Reporter_description','Partner_description','Value')]
-    #df_sankey_year[!(df_sankey_year$Partner_description %in% unique(df_sankey_year$Reporter_description)), ]
-    # df_sankey_year_countries#[1:min(100,nrow(df_sankey_year_countries)), ]
-    # df_sankey_year[, c('Reporter_description','Partner_description')]
-    # df_sankey_year
-    # dcast(df_sankey_year, Reporter_description + Partner_description ~ Value, sum)
+    df_sankey_year <- df_sankey[df_sankey$Year == input$year, c('Reporter_description','Partner_description','Value')]
+    if (input$radio.left == "Countries"){
+      df_sankey_left <- df_sankey_year[! (df_sankey_year$Reporter_description %in% non_countries), ]
+    } else {
+      df_sankey_left <- df_sankey_year[(df_sankey_year$Reporter_description %in% non_countries), ]      
+    }
+    
+    if (input$radio.right == "Countries"){
+      df_sankey_right <- df_sankey_left[! (df_sankey_left$Partner_description %in% non_countries_2), ]
+    } else {
+      df_sankey_right <- df_sankey_left[(df_sankey_left$Partner_description %in% non_countries_2), ]      
+    }
+    return(df_sankey_right)
     })
    
    # output$Bubble.Plot <- renderPlotly({
@@ -151,12 +179,29 @@ server <- function(input, output) {
               )
     })
 
+  # 
+  # output$description <- renderText({
+  #   nrow(sub_df_sankey()[, 1:2]) - nrow(unique(sub_df_sankey()[, 1:2]))
+  # })
   
-  output$description <- renderText({
-    nrow(sub_df_sankey()[, 1:2]) - nrow(unique(sub_df_sankey()[, 1:2]))
+  # output$table <- renderDataTable(sub_df_sankey())
+  
+  output$d3tree <- renderD3tree2({
+    m <- sub_df_tree()
+    tm <- treemap(
+      m[1:200, ],
+      # m[m$year >= input$year.start, ],
+      # index=c("Reporter_description", "Indicator_code"),
+      index=c("Reporter_description", "Flow_Description", "Indicator_description"),
+      vSize="Value",
+      # vColor="Flow_Code",
+      # type="Indicator_code",
+      palette="-RdGy",
+      # clickAction=paste0("Rating: ", ~rating),
+      fontsize.legend = 8,
+      format.legend = list(scientific = FALSE, big.mark = " "))
+    d3tree2(tm, rootname = "International Trade" )
   })
-  
-  output$table <- renderDataTable(sub_df_sankey())
   
 }
 
